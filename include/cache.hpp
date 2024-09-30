a55966ca8d9baf50283e2390aeb0fe6984de7f9f
Michael <mishabelov0525@gmail.com>
2024-09-30 18:52:45 +0300

    fifo works with small tests

--- include/cache.hpp
+++ include/cache.hpp
@@ -10,7 +10,7 @@ int slow_get_page_int (int key) { return key; }
 template <typename T, typename KeyT = int, typename frec = int> 
 std::ostream &operator<< (std::ostream &os, const std::list<std::tuple<KeyT, T, frec>> &cache) {
   for (auto const &i: cache) {
-    os << std::get<1>(i);
+    os << std::get<1>(i) << " ";
   }
   return os;
 }
@@ -31,14 +31,18 @@ template <typename T, typename KeyT = int, typename frec = int> class cache_t {
 
     cache_t(size_t sz) : sz_(sz) {}
 
+    void dump(KeyT key) { 
+      std::cout << "key: " << key << " small: " << small_cache << " main: " << main_cache << " ghost: " << ghost_cache << "\n"; 
+    }
+
     template <typename F> bool lookup_update(KeyT key, F slow_get_page) {
 
-      std::cout << "small: " << small_cache << "main: " << main_cache << "small: " << small_cache << "\n";
+
       auto ghost_hit = ghost_hash.find(key);
       auto main_hit = main_hash.find(key);
       auto small_hit = small_hash.find(key);
 
-      if (small_hit == small_hash.end() || main_hit == main_hash.end() || ghost_hit == ghost_hash.end()){
+      if (small_hit == small_hash.end() && main_hit == main_hash.end() && ghost_hit == ghost_hash.end()){      //key not found
         if (small_cache.size() == sz_){                                                                       
           if (std::get<2>(small_cache.back()) > 0){                                                                            
             if (main_cache.size() == sz_){
@@ -48,7 +52,7 @@ template <typename T, typename KeyT = int, typename frec = int> class cache_t {
 
             main_cache.emplace_front(small_cache.back());                                                       
             main_hash.emplace(std::get<0>(*(main_cache.begin())), main_cache.begin());               
-                                 
+            small_hash.erase(std::get<0>(small_cache.back()));                     
             small_cache.pop_back();
           }      
           else {
@@ -58,34 +62,34 @@ template <typename T, typename KeyT = int, typename frec = int> class cache_t {
             }
 
             ghost_cache.emplace_front(small_cache.back());
-            ghost_hash.emplace(std::get<0>(*(ghost_cache.begin())), ghost_cache.begin());        
+            ghost_hash.emplace(std::get<0>(*(ghost_cache.begin())), ghost_cache.begin()); 
+            small_hash.erase(std::get<0>(small_cache.back()));       
             small_cache.pop_back();
-            small_hash.erase(std::get<0>(small_cache.back()));
           }  
         }
-        else {
-          small_cache.emplace_front(key, slow_get_page(key), 0);     
-          small_hash.emplace(key, small_cache.begin());
-        }
+        
+        small_cache.emplace_front(key, slow_get_page(key), 0);     
+        small_hash.emplace(key, small_cache.begin());
+        
+        dump(key);
         return false;
       }
-
-      if (small_hit != small_hash.end())
-      {
-        std::get<2>(*(small_hit->second)) += 1; 
+      else {
+        if (small_hit != small_hash.end()){
+          std::get<2>(*(small_hit->second)) += 1; 
+        }
+        else if (main_hit != main_hash.end()) { 
+          std::get<2>(*(main_hit->second)) += 1; 
+        }
+        else {
+          main_cache.emplace_front(*(ghost_hit->second));   
+          main_hash.emplace(std::get<0>(*(main_cache.begin())), main_cache.begin());                    
+          ghost_cache.erase((ghost_hit->second));
+          ghost_hash.erase(ghost_hit);
+        }
+        dump(key);
         return true; 
       }
-      if (main_hit != main_hash.end())
-      { 
-        std::get<2>(*(main_hit->second)) += 1; 
-        return true; 
-      }
-  
-      main_cache.emplace_front(*(ghost_hit->second));   
-      main_hash.emplace(std::get<0>(*(main_cache.begin())), main_cache.begin());                    
-      ghost_cache.erase((ghost_hit->second));
-      ghost_hash.erase(ghost_hit);
-      return true; 
     }
 };
 
